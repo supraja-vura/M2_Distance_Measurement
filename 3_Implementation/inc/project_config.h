294b97c3fd9994f7d103a026ede3de39276a77d5
supraja-vura <suprajavura1234@gmail.com>
2022-04-21 20:52:35 +0530

    Update a file

--- 3_Implementation/inc/project_config.h
+++ 3_Implementation/inc/project_config.h
@@ -2,7 +2,7 @@
 #define __PROJECT_CONFIG_H__
 
 #define F_CPU 1000000 	
-#define enable            5
-#define registerselection 6
+#define enable  (PORTD5)
+#define registerselection (PORTD6)
 
 #endif 
